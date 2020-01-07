5d144db671acbb86b841f1067aaba651f3496bdc
Yuda Aditya <yudaaditya98@gmail.com>
2020-01-07 14:23:02 +0700

    Theme admin, Login

---
+++ application/views/admin/_layouts/header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>
+        Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
+    </title>
+    <!-- Favicon -->
+    <link href="<?php echo base_url('theme/argon')?>/assets/img/brand/favicon.png" rel="icon" type="image/png">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Icons -->
+    <link href="<?php echo base_url('theme/argon')?>/assets/js/plugins/nucleo/css/nucleo.css" rel="stylesheet" />
+    <link href="<?php echo base_url('theme/argon')?>/assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="<?php echo base_url('theme/argon')?>/assets/css/argon-dashboard.css?v=1.1.1" rel="stylesheet" />
+
+</head>
---
+++ application/views/admin/_layouts/js.php
@@ -0,0 +1,16 @@
+ <!--   Core   -->
+ <script src="<?php echo base_url('theme/argon') ?>/assets/js/plugins/jquery/dist/jquery.min.js"></script>
+ <script src="<?php echo base_url('theme/argon') ?>/assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+ <!--   Optional JS   -->
+ <script src="<?php echo base_url('theme/argon') ?>/assets/js/plugins/chart.js/dist/Chart.min.js"></script>
+ <script src="<?php echo base_url('theme/argon') ?>/assets/js/plugins/chart.js/dist/Chart.extension.js"></script>
+ <!--   Argon JS   -->
+ <script src="<?php echo base_url('theme/argon') ?>/assets/js/argon-dashboard.min.js?v=1.1.1"></script>
+ <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+ <script>
+     window.TrackJS &&
+         TrackJS.install({
+             token: "ee6fab19c5a04ac1a32a645abde4613a",
+             application: "argon-dashboard-free"
+         });
+ </script>
